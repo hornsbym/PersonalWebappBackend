1c0b9c717328248705b2ba09005f9b0c6eb46efa
Mitchell Hornsby <mitchell.hornsby@publicissapient.com>
2019-08-08 10:32:41 -0400

    Removed unnecessary files and directories.

--- client/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Site from './components/Site';
-import "./style.css"
-
-ReactDOM.render(<Site />, document.getElementById('root'));
